501e9f2a2a5161e5c92dfce60359a09af0bbe131
fabibot <fabulousfab.bellon@gmail.com>
2025-04-09 14:47:53 +0200

    padding formation

--- src/components/cvComposants/Formation.vue
+++ src/components/cvComposants/Formation.vue
@@ -3,7 +3,7 @@
         <h2 class="octarine-b"> Formation </h2>
         <div>
             <div v-for="diplome in formation" class="my-3">
-                <span class="cascadia-sb fond-bleu">{{ diplome.annee }}</span> - {{ diplome.nom }}
+                <span class="cascadia-sb fond-bleu p-1">{{ diplome.annee }}</span> - {{ diplome.nom }}
             </div> 
         </div>
     </div>
