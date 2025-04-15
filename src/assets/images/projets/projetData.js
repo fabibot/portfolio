b26874405ca3d4d77e9d017542ae8271b6b9cb1f
fabibot <fabulousfab.bellon@gmail.com>
2025-04-15 14:32:18 +0200

    ajout data projet

--- src/assets/images/projets/projetData.js
+++ src/assets/images/projets/projetData.js
@@ -1,7 +1,13 @@
 const projetList = [
     { nom : "Abysse", image: "abysse2.png", lien: "https://fabibot.github.io/DeambulationNumerique/page1.html"},
     { nom : "Bauhaus", image: "bauhaus2.png", lien: "https://fabibot.github.io/DeambulationNumerique/page2.html"},
-    { nom : "Time", image: "time2.png", lien: "https://fabibot.github.io/DeambulationNumerique/page3.html"}
+    { nom : "Time", image: "time2.png", lien: "https://fabibot.github.io/DeambulationNumerique/page3.html"},
+    { nom : "Close to the machine", image: "close-to-the-machine.png", lien: "https://fabibot.github.io/Close-to-the-Machine/"},
+    { nom : "Meteo", image: "meteo-api.png", lien: "https://fabibot.github.io/meteo/"},
+    { nom : "Tic tac toe", image: "tictactoe.png", lien: "https://fabibot.github.io/JeuMorpion-Odin/"}
+
+
+
 
 ];
 
--- src/components/portfolio/ProjetsIndex.vue
+++ src/components/portfolio/ProjetsIndex.vue
@@ -2,14 +2,17 @@
     <div class="projet-index container-fluid">
         <div class="p-4 d-flex">
         <RouterLink to="/cv">retour au cv</RouterLink>
-        <h1>Explorez les projets !</h1>
+        <h1>Explorez les projets</h1>
     </div>
     <div class="grid-projet">
-        <div v-for="projet in projetList" class="p-2">
-            <img :src="getImagePath(projet.image)" alt="" class="rounded">
-            <p class="text-right">
+        <div v-for="projet in projetList" class="p-4">
+            <div class="border-solid">
+                <img :src="getImagePath(projet.image)" alt="" class="rounded">
+            <p class="text-right cascadia-sb">
                 {{ projet.nom }}
             </p>
+        </div>
+
         </div>
     </div>
     </div>
@@ -34,9 +37,17 @@ function getImagePath (filename) {
     .grid-projet {
         display: grid;
         grid-template-columns: repeat(4, 25%);
-        gap: 45px;
     }
     .grid-projet img {
         width: 100%;
     }
+    .border-solid {
+        border: solid 3px black;
+        padding: 10px;
+        transition: all 0.3s;
+    }
+    .border-solid:hover {
+        box-shadow: 0px 0px 0px 7px black;
+    }
+
 </style>
