2eb99ef0ccfaa0ea126872648daf0ff99d5021d7
fabibot <fabulousfab.bellon@gmail.com>
2026-09-07 15:49:08 +0200

    MAJ du CV

--- src/mainData.js
+++ src/mainData.js
@@ -10,7 +10,7 @@ const info = {
       linkedin: { nom : "Fabiola Bellon", lien : "https://www.linkedin.com/authwall?trk=bf&trkInfo=AQHZAenncv2f1wAAAZYasofwRKf_3-rC6SGyeX6uwSoZhBLoEdN2wFJ1s-o-OeBnAwnbd92GP5j_h6b2Q_g1beQlKpmeHm8XyuWhAj17TJrlimuHiFFro_y-oEXVogQU1gj86dY=&original_referer=&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fin%2Ffabiola-bellon-66527920b%2F" },
       git: { nom : "fabibot", lien : "https://github.com/fabibot"},
     },
-    description: `Après six mois d'expérience chez Illiwap et quatre mois chez Diagram Informatique, axés sur l'intégration de maquettes pour le web, je suis à la recherche d'une nouvelle aventure en tant que développeuse front-end. Mon parcours, aussi atypique qu'enrichissant, mêlant design, graphisme et informatique, montre à quel point je ne me lasse jamais d'apprendre.`,
+    description: `Je peux gérer votre site internet de sa conception à sa mise en ligne, assurer sa maintenance, travailler son référencement naturel et créer du contenu pour vos réseaux sociaux. Plus largement, j’aime prendre en charge tout ce qu'il y a autour de la statégie digitale. Mon parcours, aussi atypique qu'enrichissant, mêlant design, graphisme et informatique, montre à quel point je suis motivé par l'apprentissage. Aujourd’hui, je souhaite mettre cette polyvalence au service de votre entreprise.`,
     // formation : [
     //       {annee: '2024', nom: 'The Odin Project, Full Stack Javascript'},
     //       {annee: '2023', nom: 'Harvard University CS50x, Introduction to Computer Science'},
@@ -32,14 +32,17 @@ const info = {
       { nom: "Bootstrap", niveau: 5 },
       { nom: "Flutter", niveau: 4 },
       { nom: "Git", niveau: 4 },
-      { nom: "HTML, CSS, JavaScript", niveau: 4 },
-      { nom: "+ Initiations à Figma", niveau: 0 }
+      { nom: "HTML, CSS, JavaScript", niveau: 5 },
+      { nom: "figma", niveau: 3 },
+      { nom: "Photoshop", niveau : 4 },
+      { nom: "Montage vidéo", niveau: 4 },
+      { nom: "+ Initiations à Wordpress", niveau: 0 }
   ],
   experience : [
-      {duree : "Octobre 2024 à Mars 2025, Illiwap, Saint-Etienne", description: "Participation au développement et à la maintenance d’un projet sous Symfony, gestion des tickets clients et correction de bugs. Ce projet à l'écosystème complet (backoffice, site web public, CRM et application mobile) m’a permis  de mieux comprendre les enjeux d’un projet à grande échelle."},
-      {duree : " mai-aôut 2024 Diagram Informatique, Saint-Etienne", description: "Création d'un site internet avec Vue.js. Intégration de maquettes avec Symfony, Twig et Bootstrap."},
-      {duree : " Janvier 2022 And Jacob, Mexique", description: "Conception de produits autour des arts de la table. Élaboration de dessins, de rendu 3D et de plans sur Rhinocéros et Keyshot."},
-      {duree : " Octobre 2020 Distorsion, Auvergne Rhône-Alpes", description: "Conception d’une identité graphique pour une recyclerie. Conception d’un logo et d’illustrations à l’aide du logiciel Illustrator."},
+      {duree : "2024 - 2025, Illiwap, Saint-Etienne", description: "Participation au développement et à la maintenance d’un projet sous Symfony, gestion des tickets clients et correction de bugs. Ce projet à l'écosystème complet (backoffice, site web public, CRM et application mobile) m’a permis  de mieux comprendre les enjeux d’un projet à grande échelle."},
+      {duree : "2024, Diagram Informatique, Saint-Etienne", description: "Création d'un site internet avec Vue.js. Intégration de maquettes avec Symfony, Twig et Bootstrap."},
+      {duree : "2022, And Jacob, Mexique", description: "Conception de produits autour des arts de la table. Élaboration de dessins, de rendu 3D et de plans sur Rhinocéros et Keyshot."},
+      {duree : "2021, Distorsion, Auvergne Rhône-Alpes", description: "Conception d’une identité graphique pour une recyclerie. Conception d’un logo et d’illustrations à l’aide du logiciel Illustrator."},
   ],
   hobbies : [
       { nom: "Escalade", description: " pratique du bloc et de la voie" },
