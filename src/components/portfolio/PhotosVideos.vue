aeee555172cc9088d56ad7455b63c48d16072f65
fabibot <fabulousfab.bellon@gmail.com>
2026-09-16 12:14:00 +0200

    prise de vue réelle

--- src/components/portfolio/PhotosVideos.vue
+++ src/components/portfolio/PhotosVideos.vue
@@ -8,27 +8,45 @@
                 Prise de vue réelle
             </h1>
         </div>
-        <div class="mx-5">
-          <p class="cascadia-el">ecrire le texte </p>
+        <!-- <div class="mx-5">
+          <p class="cascadia-el">ecrire le texte // vidéo à côté et photo en dessous</p>
+        </div> -->
+
+    <div class="mt-5 row container align-items-end">
+      <div class="col-4">
+        <img class="main-img" :src="getImagePath('P1140074-retouche.jpg')" alt="illustration principale">  
+      </div>
+      <div class="col-8">
+        <h3 class="octarine-b">Nature morte et bijoux</h3>
+        <div class="gallery-scroll scroller" @wheel="scrollHorizontal">
+          <img
+            v-for="(image, index) in gallery2"
+            :key="index"
+            :src="getImagePath(image.src)"
+              :alt="image.alt || ''"
+          />
         </div>
-
-    <div class="mt-5">
-
-      <h3 class="octarine-b">Homo Bulla</h3>
-      <div class="d-flex align-items-center">
-        <div class="gallery"  @mouseenter="startScroll" @mouseleave="stopScroll">
-            <div class="gallery-track" :style="{ transform: `translateX(-${scroll}px)` }">
-              <img
-                v-for="(image, index) in gallery"
-                :key="index"
-                :src="getImagePath(image.src)"
-                  :alt="image.alt || ''"
-              />
-            </div>
-          </div>
       </div>
     </div>
 
+    
+    <div class="mt-5 row container align-items-end">
+      <div class="col-4">
+        <img class="main-img" :src="getImagePath('diplome2.JPG')" alt="illustration principale">  
+      </div>
+      <div class="col-8">
+        <h3 class="octarine-b">Homo Bulla</h3>
+        <div class="gallery-scroll scroller" @wheel="scrollHorizontal">
+          <img
+            v-for="(image, index) in gallery1"
+            :key="index"
+            :src="getImagePath(image.src)"
+              :alt="image.alt || ''"
+          />
+        </div>
+      </div>
+    </div>
+      <div style="height: 60px;"></div>
     </div>
 
 
@@ -43,46 +61,30 @@ function getImagePath (filename) {
      return new URL(`../../assets/images/competence/photos-videos/${filename}`, import.meta.url).href;
     }
 
-const scroll = ref(0)
-let animationFrame
-let lastTime = null
+const scrollHorizontal = (event) => {
+      event.preventDefault();
 
-const speed = 70 // pixels par seconde
-
-function animate(timestamp) {
-  if (!lastTime) lastTime = timestamp
-
-  const delta = timestamp - lastTime
-  lastTime = timestamp
-
-  scroll.value += speed * delta / 1000
-
-  animationFrame = requestAnimationFrame(animate)
+    event.currentTarget.scrollLeft += event.deltaY;
 }
 
-function startScroll() {
-  if (!animationFrame) {
-    lastTime = null
-    animationFrame = requestAnimationFrame(animate)
-  }
-}
-
-function stopScroll() {
-  cancelAnimationFrame(animationFrame)
-  animationFrame = null
-  lastTime = null
-}
-
-const gallery = [
-    { src: "diplome1.JPG"},
-    { src: "diplome2.JPG"},
+const gallery1 = [
     { src: "bulle2.JPG"}, 
     { src: "diplome3.JPG"},
     { src: "diplome4.JPG"},
     { src: "bulle3.JPG"},
-    { src: "bulle1.JPG"},
+    { src: "diplome1.JPG"},
     { src: "bulle4.JPG"},
     { src: "bulle5.JPG"},
+    { src: "bulle1.JPG"},
+]
+
+const gallery2 = [
+    { src: "P1140053-retouche3.jpg"},
+    { src: "P1130910-retouche7.jpg"},
+    { src: "P1130988-retouche.jpg"},
+    { src: "P1140056-retouche.jpg"},
+    { src: "P1140070.JPG"},
+    { src: "P1140104-retouche4.jpg"}, 
 ]
 
 </script>
@@ -96,17 +98,27 @@ const gallery = [
   cursor: grab;
 }
 
-.gallery-track {
-  display: flex;
-  width: max-content;
-  gap: 20px;
-}
-
-.gallery-track img {
-  width: 260px;
-  height: 320px;
+.main-img {
+   width: 100%;
+  max-height: 500px;
   object-fit: cover;
   flex-shrink: 0;
 }
 
+.gallery-scroll {
+  display: flex;
+  gap: 15px;
+  overflow-x: scroll;
+}
+
+.gallery-scroll img {
+  width: 200px;
+  height: 260px;
+  object-fit: cover;
+  flex-shrink: 0;
+}
+
+.scroller {
+  scrollbar-color: #4b5a66b3 #c4c4c400;
+}
 </style>
